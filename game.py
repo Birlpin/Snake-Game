8fd7d3410f1273efa4f2490682d5ed0ade54972f
yaroslavbatyzhin <y.batyzhin@gmail.com>
2025-11-16 17:36:52 -0700

    Update: Start Screen Creation

---
+++ game.py
@@ -0,0 +1,6 @@
+class game():
+    def __init__(self):
+        self.gaming = False
+
+    def draw_window(self, window):
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import game
 import starterscreen
 import pygame
+
 class main():
     def __init__(self):
         pygame.init()
@@ -11,25 +13,34 @@ class main():
 
         self.window = pygame.display.set_mode((window_x, window_y))
 
+        # init the start class
+        self.start = starterscreen.start()
+        self.gaming = game.game().gaming
+
         main.run(self)
+
 
     def run(self):
 
         run = True
         while run:
 
-            if starterscreen.start().startin == True:
-                starterscreen.start().draw_screen(self.window)
-
-
+            # starter screen draw
+            if self.start.startin == True:
+                self.start.draw_screen(self.window)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
 
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    click_pos = event.pos
+                # IF in title screen
+                if self.start.startin == True:
+                    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                        click_pos = pygame.mouse.get_pos()
 
+                        if self.start.startbutton.collidepoint((click_pos)):
+                            self.start.startin = False
+                            self.gaming = True
 
             pygame.display.update()
             self.clock.tick(30)
